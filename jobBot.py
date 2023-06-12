2a288c6924cdfbd4dc00657bf06c92b4aa231431
shashankreddybanda <shashank24140544@gmail.com>
2023-06-12 09:00:09 -0700

    Skeleton code

--- jobBot.py
+++ jobBot.py
@@ -1,4 +1,11 @@
+from selenium import webDriver
+from selenium.webdirver.common.by import By
+from selenium.common.exception import NoSuchElementException
+
+
 print("this is a job bot")
 
+
 def apply():
+    
     print("applying")
